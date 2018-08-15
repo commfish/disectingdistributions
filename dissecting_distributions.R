930998a517157f30b07880e2d3f7d8789d089870
Sages11 <power_sarah@yahoo.com>
2018-08-15 14:47:35 -0800

    Stylized coding edits + automation of estimated means with three distributions.

--- dissecting_distributions.R
+++ dissecting_distributions.R
@@ -7,54 +7,50 @@
 library(tidyverse)
 library(mixdist)
 library(lubridate)
-library(scales)
-library(ggplot2)
 library(here)
 #source('code/functions.R')
 
 # data ----
-weir_data <- read_csv('data/ChigISGrunappt2006-2017.csv') %>% 
+read_csv('../data/ChigISGrunappt2006-2017.csv') %>% 
   dplyr::select(-X9) %>%
-  dplyr::rename(date = Date, 
-                prop_early_genetics = Propotionearly, 
-                early_esc_genetics = Earlyesc, 
-                late_esc_genetics = Lateesc,
-                early_catch_genetics = Earlycatch,
-                late_catch_genetics = Latecatch,
-                early_total_genetics = Earlytotal,
-                late_total_genetics = Latetotal)%>%
-  dplyr::mutate(esc = early_esc_genetics + late_esc_genetics,
-                catch = early_catch_genetics + late_catch_genetics)
-weir_data$date <- as.Date(weir_data$date ,"%m/%d/%y")
+  rename(date = Date, 
+         prop_early_genetics = Propotionearly, 
+         early_esc_genetics = Earlyesc, 
+         late_esc_genetics = Lateesc,
+         early_catch_genetics = Earlycatch,
+         late_catch_genetics = Latecatch,
+         early_total_genetics = Earlytotal,
+         late_total_genetics = Latetotal) %>%
+  mutate(esc = early_esc_genetics + late_esc_genetics,
+         catch = early_catch_genetics + late_catch_genetics,
+         date = mdy(date),
+         date_num = as.numeric(date)) -> weir_data # convert the Date to its numeric equivalent
+# Filter out just 2006 data.
+weir_data %>% 
+  filter(year(date)==2006) -> weir_2006
 
-glimpse(weir_data)
 
-weir_data <- weir_data %>% filter(year(weir_data$date)==2006)
-unique(year(weir_data$date))
+#Graph daily weir escapement for a year ----
+ggplot(weir_2006, aes(date_num, esc)) + 
+  geom_line() +
+  labs(title = "Daily weir passage", x = "date in number format", y = "Number of fish")
 
-
-#Graph data ----
-
-# convert the Date to its numeric equivalent
-# Note that Dates are stored as number of days internally,
-# One can convert back and forth since dates don't lend well to numeric algorithms. 
-weir_data$date_num <- as.numeric(weir_data$date)
 
 # Create a data frame whose first column are the dates in numeric format
 # and whose second column are the frequencies. 
 # This is required for fitting the mixture. See mixdata {mixdist}
-df <- data.frame(mid=weir_data$date_num, cou=weir_data$esc)  
-head(df)
+weir_2006 %>% 
+  dplyr::select(date_num, esc) -> df
 
-# Graph a plot of daily weir passage
-ggplot(data = weir_data, aes(x = date_num, y = esc)) + 
-  geom_line() +
-  labs(title = "Daily weir passage", x = "date in number format", y = "Number of fish")
 
 # Guess at mean "number date" and sigmas ----
 #Using the above graph guess at Means (numeric date) in Ascending Order, 
 #and the associated Sigmas and distribution.  
-guemea <- c(13320, 13350, 13370)#c(17340, 17375, 17410)  
+
+#Perhaps just use mean numeric date +- 30 in ascending order.IF that doesn't work hard code a guess.
+guemea <- c(mean_date - 30, mean_date, mean_date + 30)
+
+#guemea <- # for 2006: c(13320, 13350, 13370)# for 2017: c(17340, 17375, 17410)  
 guesig <- c(10, 10, 10)  
 guedis <- "norm"  
 (fitpro <- mix(as.mixdata(df), mixparam(mu=guemea, sigma=guesig), dist=guedis))  
