f96e533644050f64992e567199fe432015e7d623
Sages11 <power_sarah@yahoo.com>
2018-08-16 11:43:19 -0800

    adding/changing functions in hopes of using other distributions more easily.

--- code/dissecting_distributions.R
+++ code/dissecting_distributions.R
@@ -22,18 +22,21 @@ read_csv('data/ChigISGrunappt2006-2017.csv') %>%
          late_total_genetics = Latetotal) %>%
   mutate(esc = early_esc_genetics + late_esc_genetics,
          catch = early_catch_genetics + late_catch_genetics,
+         run = esc + catch, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
          date = mdy(date),
-         date_num = as.numeric(date)) # convert the Date to its numeric equivalent
--> weir_data 
+         date_num = as.numeric(date)) -> weir_data # convert the Date to its numeric equivalent
 
-
-
-# Filter out just on years data.
 
 #2006
-weir_data %>% filter(year(date)==2006) -> weir_2006
+#weir_data %>% filter(year(date)==2006) -> weir_2006
+weir_2006 <- data_prep(weir_data, 2006)
 graph_year(weir_2006)
-distribution_estimation(weir_2006)
+fitpro <- distribution_estimation(weir_2006)
+#fitpro <- mix(weir_2006,mixparam(c(13320, 13350),.5),"gamma",mixconstr(consigma="CCV"))
+dist_plot(fitpro)
+
+auto_year(weir_data, 2006)
+
 
 #2007
 weir_data %>% filter(year(date)==2007) -> weir_2007
@@ -43,8 +46,8 @@ distribution_estimation(weir_2007)
 #2008
 weir_data %>% filter(year(date)==2008) -> weir_2008
 graph_year(weir_2008)
-distribution_estimation(weir_2008)
-distribution_estimation(weir_2008, 3, mean_guess_given = c(14050, 14080, 14110 ))
+distribution_estimation(weir_2008) #THis didn't work very well
+distribution_estimation(weir_2008, 3, mean_guess_given = c(14050, 14080, 14110 )) # This worked better
 
 auto_year(weir_data, 2008)
 
@@ -55,3 +58,4 @@ auto_year(weir_data, 2013)
 auto_year(weir_data, 2014)
 auto_year(weir_data, 2015)
 auto_year(weir_data, 2016)
+auto_year(weir_data, 2017)
--- code/distribution_functions.R
+++ code/distribution_functions.R
@@ -11,20 +11,23 @@ library(lubridate)
 #source('code/functions.R')
 
 # functions ----
+data_prep <- function(df, year_wanted){
+  df %>% 
+    filter(year(date)==year_wanted) %>%
+  # Create a data frame whose first column are the dates in numeric format
+  # and whose second column are the frequencies. 
+  # This is required for fitting the mixture. See mixdata {mixdist}
+    dplyr::select(date_num, run) -> df
+}
+
 graph_year <- function(df){
-  #Graph daily weir escapement for a year ----
-  ggplot(df, aes(date_num, esc)) + 
+  #Graph daily weir run = escapement + catch for a year ----
+  ggplot(df, aes(date_num, run)) + 
     geom_line() +
     labs(title = "Daily weir passage", x = "date in number format", y = "Number of fish")
 }
 
-distribution_estimation <- function(df, num_of_distributions = 3, mean_guess_given , sigma_guess_given, distibution_guess = 'norm'){
-  
-  # Create a data frame whose first column are the dates in numeric format
-  # and whose second column are the frequencies. 
-  # This is required for fitting the mixture. See mixdata {mixdist}
-  df %>% 
-    dplyr::select(date_num, esc) -> df
+distribution_estimation <- function(df, num_of_distributions = 3, mean_guess_given , sigma_guess_given, distibution_guess = 'gamma'){
   
   if(missing(mean_guess_given)) {
     mean_guess = c(mean(df$date_num) -30, mean(df$date_num), mean(df$date_num)+30) #currently the default is for three distributions.
@@ -37,22 +40,26 @@ distribution_estimation <- function(df, num_of_distributions = 3, mean_guess_giv
     sigma_guess = sigma_guess_given
   }
   
-  
   (fitpro <- mix(as.mixdata(df), mixparam(mu=mean_guess, sigma=sigma_guess), dist=distibution_guess))  
   
+}
+
+dist_plot <- function (fitpro){
   #Plot the results  
   plot(fitpro, main="Fit Distributions for a year" )  
   grid()  
   legend("topright", lty=1, lwd=c(1, 1, 2), c("Original Distribution to be Fit", "Individual Fitted Distributions", "Fitted Distributions Combined"), col=c("blue", "red", rgb(0.2, 0.7, 0.2)), bg="white")  
   
   #Estimated mean date and sigmas.
-  summary(fitpro)
+  summary(fitpro) 
 }
+  
 
 auto_year <- function (df, year_wanted) {
-  df %>% filter(year(date) == year_wanted ) -> df_year
+  df_year <- data_prep(df, year_wanted ) 
   graph_year(df_year)
-  distribution_estimation(df_year)
+  fitpro <- distribution_estimation(df_year) 
+  dist_plot(fitpro)
 }
 
 
