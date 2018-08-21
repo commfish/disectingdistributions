f45e9118f1878602248395abf3bbbad5833459a6
Sages11 <power_sarah@yahoo.com>
2018-08-21 13:45:43 -0800

    Changed distribution_estimation function to distribution_estimation_norms Began working on a similar weibul function Also added dnorm fit percent_dist pnormfit tails_difference tails_equal_date

--- code/distribution_functions.R
+++ code/distribution_functions.R
@@ -27,7 +27,7 @@ graph_year <- function(df){
     labs(title = "Daily weir passage", x = "date in number format", y = "Number of fish")
 }
 
-distribution_estimation <- function(df, num_of_distributions = 3, mean_guess_given , sigma_guess_given, distibution_guess = 'gamma'){
+distribution_estimation_norms <- function(df, num_of_distributions = 3, mean_guess_given , sigma_guess_given, distibution_guess = 'gamma'){
   
   if(missing(mean_guess_given)) {
     mean_guess = c(mean(df$date_num) -30, mean(df$date_num), mean(df$date_num)+30) #currently the default is for three distributions.
@@ -40,9 +40,27 @@ distribution_estimation <- function(df, num_of_distributions = 3, mean_guess_giv
     sigma_guess = sigma_guess_given
   }
   
-  (fitpro <- mix(as.mixdata(df), mixparam(mu=mean_guess, sigma=sigma_guess), dist=distibution_guess))  
+  (fitpro <- mix(as.mixdata(df), mixparam(mu=mean_guess, sigma=sigma_guess), dist=distibution_guess, iterlim=5000))  
   
 }
+
+distribution_estimation_weibul <- function(df, num_of_distributions = 3, mean_guess_given , sigma_guess_given, distibution_guess = 'weibull'){
+  
+  if(missing(mean_guess_given)) {
+    mean_guess = c(mean(df$date_num) -30, mean(df$date_num), mean(df$date_num)+30) #currently the default is for three distributions.
+  } else {
+    mean_guess = mean_guess_given
+  }
+  if(missing(sigma_guess_given)) {
+    sigma_guess = rep(10, each = num_of_distributions)
+  } else {
+    sigma_guess = sigma_guess_given
+  }
+  
+  (fitpro <- mix(as.mixdata(df), mixparam(mu=mean_guess, sigma=sigma_guess), dist=distibution_guess))  #, iterlim=5000
+  
+}
+
 
 dist_plot <- function (fitpro){
   #Plot the results  
@@ -62,4 +80,56 @@ auto_year <- function (df, year_wanted) {
   dist_plot(fitpro)
 }
 
+dnormfit <- function(fit, x, dist_num){
+  fit$parameters$pi[dist_num]*dnorm(x, fit$parameters$mu[dist_num],fit$parameters$sigma[dist_num])
+}
+
+percent_dist <- function(fit, x, dist_num = 1){
+  dnormfit(fit, x, dist_num)/(dnormfit(fit, x, 1)+dnormfit(fit, x, 2)+dnormfit(fit, x, 3)) 
+}
+
+pnormfit <- function(fit, x, dist_num =1){
+  fit$parameters$pi[dist_num]*pnorm(x, fit$parameters$mu[dist_num],fit$parameters$sigma[dist_num])
+}
+
+tails_difference <- function(fit, x, dist_a =1, dist_b =2){
+  difference <- abs(pnormfit(fit, x, 1) + 1 - pnormfit(fit, x, 2))
+}
+
+tails_equal_date <- function(fit, dist_a =1, dist_b =2){
+  # find X (date) when pnorm(dist 1) = 1 - pnorm(dist 2) taking into account the proporiton (pi)
+  # each distribution is of the whole multinomial distribution
+
+  #find x where
+  pnormfit(fitpro, 13338, 2) #is about equal to  
+  1 - pnormfit(fitpro, 13338+1, 1) 
+  # AKA 
+  pnormfit(fitpro, 13338+1, 2) - 1 + pnormfit(fitpro, 13338+1, 1) # is as small as possible. 
+  
+  # start with x (date) halfway between 2 means
+  current_x <- floor((fit$parameters$mu[dist_a]+fit$parameters$mu[dist_b])/2)
+  difference_current <- tails_difference(fit, current_x)
+  difference_new <- tails_difference(fit, current_x+1)
+  
+  # if the next day the difference is greater 
+  if(difference_current < difference_new ){
+    # then consider the day before
+    new_x = current_x - 1
+    difference_new = tails_difference(fit, new_x)
+    while (difference_new < difference_current){ #keep going back a day until the smallest difference is found
+      difference_current <- difference_new
+      current_x <- new_x
+      new_x <- new_x - 1
+      difference_new <- tails_difference(fit, new_x)
+    } } else{ # difference_new <= difference_current # keep going forward a day until the smallest difference is found
+      while (difference_new < difference_current){
+        difference_current <- difference_new
+        current_x <- new_x
+        new_x = new_x + 1
+        difference_new = tails_difference(fit, new_x)
+      }
+    }
+  
+  return(as.Date.numeric(current_x)) # date 
+}
 
