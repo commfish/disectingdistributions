d6a19b767371546061e6be4fa3b2152f53dd9502
Sages11 <power_sarah@yahoo.com>
2018-08-22 11:26:25 -0800

    Clean up and fit distributions for various years.

--- code/dissecting_distributions.R
+++ code/dissecting_distributions.R
@@ -32,97 +32,39 @@ read_csv('data/ChigISGrunappt2006-2017.csv') %>%
 #2006
 weir_2006 <- data_prep(weir_data, 2006)
 graph_year(weir_2006)
- fit <-fitpro <- distribution_estimation_norms(weir_2006)
-#fitpro <- mix(weir_2006,mixparam(c(13325, 13350, 13380),.5),"weibull")
-#(fitpro <- mix(as.mixdata(weir_2006), mixparam(mu=c(13325, 13350, 13380), sigma=c(10,15,10)), dist="norm")) 
-dist_plot(fitpro)
-summary(fit)
-fit <-fitpro <- distribution_estimation_weibull(weir_2006, 2, mean_guess_given = c(13325, 13350, 13380), c(10,10,10),  distibution_guess = 'weibull')
-distribution_estimation_weibull <- function(df, num_of_distributions = 2, mean_guess_given , sigma_guess_given, distibution_guess = 'weibull')
-  fit <-fitpro <- distribution_estimation_weibull(weir_2006)
+(fit <- mix(as.mixdata(weir_2006), mixparam(mu=c(13335, 13355), sigma= c(10,10)), dist= 'gamma', iterlim=5000))  
+(fit <- mix(as.mixdata(weir_2006), mixparam(mu=c(13335, 13355, 13380), sigma= c(10,10,20)), dist= 'gamma', iterlim=5000))  
+(fit <- mix(as.mixdata(weir_2006), mixparam(mu=c(13335, 13355), sigma= c(15,15)), dist= 'weibull', iterlim=5000))  
+(fit <- mix(as.mixdata(weir_2006), mixparam(mu=c(13335, 13355, 13380), sigma= c(10,10,20)), dist= 'weibull', iterlim=5000))  
 
-pnormfit(fit, 13338, 2)
-pnormfit(fit, 13338, 1)
-1-pnormfit(fit, 13338, 1)
-
-tails_difference(fit, 13338, 1, 2)
-
-percent_dist <- function(fit, x, dist_num = 1){
-  dnormfit(fit, x, dist_num)/(dnormfit(fit, x, 1)+dnormfit(fit, x, 2)+dnormfit(fit, x, 3)) 
-}
-
-percent_dist(fitpro, 13338, 1)
-as.Date.numeric(13330)
-
-auto_year(weir_data, 2006)
-tails_equal_date(fitpro, dist_a =1, dist_b =2)
-
-weir_2017 <- data_prep(weir_data, 2017)
-graph_year(weir_2017)
-fit2017 <- distribution_estimation(weir_2017)
-dist_plot(fit2017)
-(fitpro <- mix(as.mixdata(weir_2017), mixparam(mu=c(17338, 1717370), sigma=c(10,15)), dist="weibull")) 
-
-
-tails_equal_date(fitpro, dist_a =1, dist_b =2)
-
-pnormfit <- function(fit, x, dist_num =1){
-  fit$parameters$pi[dist_num]*pnorm(x, fit$parameters$mu[dist_num],fit$parameters$sigma[dist_num])
-}
-
-tails_difference <- function(fit, x, dist_a =1, dist_b =2){
-  abs(pnormfit(fit, x, 1) - (1 - pnormfit(fit, x, 2)))
-}
-
-tails_equal_date <- function(fit, dist_a =1, dist_b =2){
-  # find X (date) when pnorm(dist 1) = 1 - pnorm(dist 2) taking into account the proporiton (pi)
-  # each distribution is of the whole multinomial distribution
-  
-  #find x where
-  pnormfit(fitpro, 13338, 2) #is about equal to  
-  1 - pnormfit(fitpro, 13338, 1) 
-  # AKA 
-  pnormfit(fitpro, 13338-1, 2) - 1 + pnormfit(fitpro, 13338-1, 1) # is as small as possible. 
-  
-  # start with x (date) halfway between 2 means
-  current_x <- floor((fit$parameters$mu[1]+fit$parameters$mu[1])/2)
-  difference_current <- tails_difference(fit, current_x)
-  difference_new <- tails_difference(fit, current_x+1)
-  
-  # if the next day the difference is greater 
-  if(difference_current < difference_new ){
-    # then consider the day before
-    new_x = current_x - 1
-    difference_new = tails_difference(fit, new_x)
-    while (difference_new < difference_current){ #keep going back a day until the smallest difference is found
-      difference_current <- difference_new
-      current_x <- new_x
-      new_x <- new_x - 1
-      difference_new <- tails_difference(fit, new_x)
-    } } else{ # difference_new <= difference_current # keep going forward a day until the smallest difference is found
-      while (difference_new < difference_current){
-        difference_current <- difference_new
-        current_x <- new_x
-        new_x = new_x + 1
-        difference_new = tails_difference(fit, new_x)
-      }
-    }
-  
-  return(current_x) #(as.Date.numeric(current_x)) # date 
-}
-
+dist_plot(fit)
 
 
 #2007
-weir_data %>% filter(year(date)==2007) -> weir_2007
+weir_2007 <- data_prep(weir_data, 2007)
 graph_year(weir_2007)
-distribution_estimation(weir_2007)
+fit <- distribution_estimation_norms(weir_2007)
+(fit <- mix(as.mixdata(weir_2007), mixparam(mu=c(13685, 13712), sigma= c(10,10)), dist= 'gamma', iterlim=5000)) 
+(fit <- mix(as.mixdata(weir_2007), mixparam(mu=c(13685, 13712, 13749), sigma= c(10,10,10)), dist= 'weibull', iterlim=5000)) 
+(fit <- mix(as.mixdata(weir_2007), mixparam(mu=c(13685, 13712), sigma= c(10,10)), dist= 'weibull', iterlim=5000)) 
 
 #2008
-weir_data %>% filter(year(date)==2008) -> weir_2008
+weir_2008 <- data_prep(weir_data, 2008)
 graph_year(weir_2008)
-distribution_estimation(weir_2008) #THis didn't work very well
-distribution_estimation(weir_2008, 3, mean_guess_given = c(14050, 14080, 14110 )) # This worked better
+fit <- distribution_estimation_norms(weir_2008) #THis didn't work very well
+fit <- distribution_estimation(weir_2008, 3, mean_guess_given = c(14050, 14080, 14110 )) # This worked better
+(fit <- mix(as.mixdata(weir_2008), mixparam(mu=c(14057, 14082, 14107), sigma= c(10,10,10)), dist= 'weibull', iterlim=5000)) #THis didn't work very well
+(fit <- mix(as.mixdata(weir_2008), mixparam(mu=c(14057, 14082), sigma= c(10,10)), dist= 'weibull', iterlim=5000)) #THis didn't work very well
+
+dist_plot(fit)
+
+#2010
+weir_2010 <- data_prep(weir_data, 2010)
+graph_year(weir_2010)
+fit <- distribution_estimation_norms(weir_2010)
+(fit <- mix(as.mixdata(weir_2010), mixparam(mu=c(14785, 14800), sigma= c(10,10)), dist= 'gamma', iterlim=5000)) 
+(fit <- mix(as.mixdata(weir_2010), mixparam(mu=c(14785, 14800), sigma= c(10,10)), dist= 'weibull', iterlim=5000)) ##THis didn't work
+(fit <- mix(as.mixdata(weir_2010), mixparam(mu=c(14785, 14800, 14835), sigma= c(10,10,10)), dist= 'weibull', iterlim=5000)) #THis didn't work
 
 auto_year(weir_data, 2006)
 auto_year(weir_data, 2007)
