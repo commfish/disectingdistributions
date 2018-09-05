316047bf2c3759b06a98e7d0187b395b25d60496
Sages11 <power_sarah@yahoo.com>
2018-09-05 14:49:59 -0800

    Refining graph display.

--- code/distribution_functions.R
+++ code/distribution_functions.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(mixdist)
 library(lubridate)
+library(gridExtra) #for multiple plots on a page
 #library(here)
 #source('code/functions.R')
 
@@ -244,42 +245,6 @@ percent_dist <- function(fit, x, dist_num = 1){
 
 pnormfit <- function(fit, x, dist_num =1){
   fit$parameters$pi[dist_num]*pnorm(x, fit$parameters$mu[dist_num],fit$parameters$sigma[dist_num])
-}
-
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
 }
 
 #This function still needs work. 
