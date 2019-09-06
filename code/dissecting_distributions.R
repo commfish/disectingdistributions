b8c303971f3ac98b868f79c4b721ce05d1b06709
Power <sarah.power@dfg.alaska.local>
2019-09-06 11:00:38 -0800

    Putting output in a neat df.

--- code/dissecting_distributions.R
+++ code/dissecting_distributions.R
@@ -27,7 +27,7 @@ chig_data <- read_csv('data/ChigISGrunappt2006-2017catch.by.district.csv') %>%
          chignik = Chignik_Hook_Kujulik) %>%
   mutate(esc = early_esc_genetics + late_esc_genetics,
          #catch = early_catch_genetics + late_catch_genetics,
-         catch_chignik = Lagoon, # + chignik,
+         catch_chignik = Lagoon, # + chignik + Kumlik,
          run = esc + catch_chignik, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
          run_early_gen = prop_early_genetics*run,
          date = mdy(Date),
@@ -61,29 +61,35 @@ fix_date_est <-weir_data %>%
   dplyr::group_by(year) %>%
   dplyr::summarise(sum_july4 = sum(run))
 
-early_look(weir_data, 2006) 
-early_look(weir_data, 2007) 
-early_look(weir_data, 2008) 
-early_look(weir_data, 2010) 
-early_look(weir_data, 2011) 
-early_look(weir_data, 2012) 
-early_look(weir_data, 2013) 
-early_look(weir_data, 2014) 
-early_look(weir_data, 2015) 
-early_look(weir_data, 2016) 
-early_look(weir_data, 2017) 
+e06 <- early_look(weir_data, 2006) 
+e07 <- early_look(weir_data, 2007) 
+e08 <- early_look(weir_data, 2008) 
+e10 <- early_look(weir_data, 2010) 
+e11 <- early_look(weir_data, 2011) 
+e12 <- early_look(weir_data, 2012) 
+e13 <- early_look(weir_data, 2013) 
+e14 <- early_look(weir_data, 2014) 
+e15 <- early_look(weir_data, 2015) 
+e16 <- early_look(weir_data, 2016) 
+e17 <- early_look(weir_data, 2017) 
 
-early_look(chig_data, 2006) 
-early_look(chig_data, 2007) 
-early_look(chig_data, 2008) 
-early_look(chig_data, 2010) 
-early_look(chig_data, 2011) 
-early_look(chig_data, 2012) 
-early_look(chig_data, 2013) 
-early_look(chig_data, 2014) 
-early_look(chig_data, 2015) 
-early_look(chig_data, 2016) 
-early_look(chig_data, 2017) 
+p_df <- rbind(e06, e07, e08, e10, e11, e12, e13, e14, e15, e16, e17)
+genetics_early_summary <- cbind(year_vector, p_df)
+
+e06 <- early_look(chig_data, 2006) 
+e07 <- early_look(chig_data, 2007) 
+e08 <- early_look(chig_data, 2008) 
+e10 <- early_look(chig_data, 2010) 
+e11 <- early_look(chig_data, 2011) 
+e12 <- early_look(chig_data, 2012) 
+e13 <- early_look(chig_data, 2013) 
+e14 <- early_look(chig_data, 2014) 
+e15 <- early_look(chig_data, 2015) 
+e16 <- early_look(chig_data, 2016) 
+e17 <- early_look(chig_data, 2017) 
+
+p_df <- rbind(e06, e07, e08, e10, e11, e12, e13, e14, e15, e16, e17)
+genetics_early_summary <- cbind(year_vector, p_df)
 
 for(i in year_vector ){
   distribution_estimation_norms_SEQ_n1(chig_data, i)
@@ -177,13 +183,13 @@ year_stats(chig_data, 2017)
 
 
 
-#2006 in detail
-weir_2006 <- data_prep(weir_data, 2008)
-#write.csv(weir_2006, file = "data/weir_2006.csv")
+#2008 in detail
+df08 <- data_prep(weir_data, 2008)
+#write.csv(df08, file = "data/df08.csv")
 
-weir_2006<- as.mixdata(weir_2006)
-(fit <- mix(as.mixdata(weir_2006), mixparam(mu=c(175, 205, 240), sigma= c(10,10,10)),constr = mixconstr(consigma="SEQ"), dist= 'gamma', iterlim=5000)) 
-(fit<- mix(as.mixdata(weir_2006), mixparam(mu=c(175, 205, 240), sigma= c(10,10,10)), constr = mixconstr(consigma="SEQ"), dist='weibull'))  #, iterlim=5000
+df08<- as.mixdata(df08)
+(fit <- mix(as.mixdata(df08), mixparam(mu=c(175, 205, 240), sigma= c(10,10,10)),constr = mixconstr(consigma="SEQ"), dist= 'gamma', iterlim=5000)) 
+(fit<- mix(as.mixdata(df08), mixparam(mu=c(175, 205, 240), sigma= c(10,10,10)), constr = mixconstr(consigma="SEQ"), dist='weibull'))  #, iterlim=5000
 mean_guess <- c(fit$parameters$mu[1],fit$parameters$mu[2],fit$parameters$mu[3])
 plot(fit)
 fit$parameters$mu[1]-1
@@ -192,9 +198,9 @@ dist_plot(fit, 2006)
 percent_dist(fit, 191) #date when fifty percent is early run. According to distributions: July 10 
 
 year_stats(weir_data, 2006)
-weir_2006$dist_percent <- percent_dist(fit, weir_2006$day_of_year)
-weir_2006$run_dis <- weir_2006$dist_percent*weir_2006$run
-weir_2006$cum <- cumsum(weir_2006$run_dis)
+df08$dist_percent <- percent_dist(fit, df08$day_of_year)
+df08$run_dis <- df08$dist_percent*df08$run
+df08$cum <- cumsum(df08$run_dis)
 
 
 percent_dist(fit, 178) #date when fifty percent is early run. According to distributions? 
@@ -264,7 +270,7 @@ ggplot(data = fitpro$mixdata, aes(fitpro$mixdata$day_of_year))+
   scale_y_continuous(breaks = NULL)
 
 
-#possilbe help here:
+#possible help here:
 #https://stackoverflow.com/questions/5688082/overlay-histogram-with-density-curve
 # create some data to work with
 x = rnorm(1000);
@@ -278,5 +284,7 @@ p0 = qplot(x, geom = 'blank') +
   theme(legend.position = c(0.85, 0.85))
 
 print(p0)
-
-
+ggplot(data = fitpro$mixdata, aes(day_of_year))+
+  geom_density()
+geom_freqpoly( aes(run), freq =TRUE)
+hist(data = fitpro$mixdata, freq = TRUE)
