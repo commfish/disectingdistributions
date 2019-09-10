714e9c6cf1439fb56c0e1b280cf0f7b52b273e7d
Power <sarah.power@alaska.gov>
2019-09-10 15:20:11 -0800

    Adding new and improved data. 2007 had data changes 2018 was new.
    Working on function to create data frames where the run is defined in a particular way.

--- code/dissecting_distributions.R
+++ code/dissecting_distributions.R
@@ -14,19 +14,52 @@ source('code/distribution_functions.R')
 
 # data ----
 # Chig_data only uses Chignik Bay District harvest
+data2018 <- read_csv('data/ChigISGrunappt2006-2017catch.by.district9-3-19.csv') %>% 
+  dplyr::select(-Earlycatch, -Latecatch, -Earlytotal, -Latetotal) %>%
+  dplyr::rename(
+    prop_early_genetics = Propotionearly, 
+    early_esc_genetics = Earlyesc, 
+    late_esc_genetics = Lateesc) %>%
+  mutate(esc = early_esc_genetics + late_esc_genetics,
+         date = mdy(Date),
+         year = year(date),
+         day_of_year = yday(date)) # %>% #-> chig_data # convert the Date to its numeric equivalent 
+
+#This works, but isn't elegant. See function "define_harvest" below where I tried for a more elegant solution
+df18lagoon <- data2018 %>% 
+  mutate(harvest = lagoon_272_10, # This is where you change things to look at different harvest data sets. 
+         run = esc + harvest, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
+         run_early_gen = prop_early_genetics*run)
+
+df18ocdb_272_20 <- data2018 %>% 
+  mutate(harvest = lagoon_272_10 + ocdb_272_20, # This is where you change things to look at different harvest data sets. 
+         run = esc + harvest, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
+         run_early_gen = prop_early_genetics*run)
+
+df18ocdb_272_30 <- data2018 %>% 
+  mutate(harvest = lagoon_272_10 + ocdb_272_20 + ocdb_272_30, # This is where you change things to look at different harvest data sets. 
+         run = esc + harvest, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
+         run_early_gen = prop_early_genetics*run)
+
+#Cannot seem get this to work.  
+define_harvest() <- function(df = data2018, start_area = lagoon_272_10, end_area = lagoon_272_10){
+  df <- df %>% 
+    mutate(harvest = sum(df$start_area:df$end_area), # This is where you change things to look at different harvest data sets. 
+          run = esc + harvest, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
+          run_early_gen = prop_early_genetics*run)
+  return(df)
+}
+
+define_harvest
+
 chig_data <- read_csv('data/ChigISGrunappt2006-2017catch.by.district.csv') %>% 
   dplyr::select(-Earlycatch, -Latecatch, -Earlytotal, -Latetotal) %>%
-  dplyr::rename(#date = Date,
+  dplyr::rename(
          prop_early_genetics = Propotionearly, 
          early_esc_genetics = Earlyesc, 
          late_esc_genetics = Lateesc,
-         #early_catch_genetics = Earlycatch,
-         #late_catch_genetics = Latecatch,
-         #early_total_genetics = Earlytotal,
-         #late_total_genetics = Latetotal,
          chignik = Chignik_Hook_Kujulik) %>%
   mutate(esc = early_esc_genetics + late_esc_genetics,
-         #catch = early_catch_genetics + late_catch_genetics,
          catch_chignik = Lagoon, # + chignik + Kumlik, This is where you change things to look at different harvest data sets. 
          run = esc + catch_chignik, #catch is our estimate of what would have occured at the wier had there been no fishing, based on migration timing studies.
          run_early_gen = prop_early_genetics*run,
